5b8b8101555129bd214d7575ccedc96e2a2e7212
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-30 10:13:45 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def poisson(lam) :
+  # Your code to generate a poisson random variable goes here
+  
+  
+  
+# This will print out some of  random variables generated using your code
+print( poisson(2), poisson(2), poisson(2), poisson(2) )
+  
